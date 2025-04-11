abb01518f93a23810894ff120ba4ba96d0453816
Luis Machado Reis <luis.reis@singularideas.com.br>
2025-04-11 15:49:16 -0300

    Fix unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { FlightList } from './flights/ui/components/FlightList';
 import { FlightService } from './flights/application/FlightService';
